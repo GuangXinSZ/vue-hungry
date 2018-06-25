ed30d7794b7208a02c4f26f109f8e14bb0fef13b
xin <1446854524@qq.com>
2018-06-26 00:20:01 +0800

    js

--- VueSell/src/components/HelloWorld.vue
+++ VueSell/src/components/HelloWorld.vue
@@ -1,17 +1,31 @@
 <template>
-  <div class="hello">
-    项目初始化中
+  <div class="app">
+   <commit-header></commit-header>
+    <div class="content">
+      <ul class="conItem">
+        <li class="itemList">商品</li>
+        <li class="itemList">评价</li>
+        <li class="itemList">商家</li>
+      </ul>
+    </div>
+    <div class="footer">
+        商品显示
+    </div>
   </div>
 </template>
 
 <script>
 // import axios from 'axios'
+import CommitHeader from 'temp/top/Header'
 export default {
   name: 'HelloWorld',
   data () {
     return {
       msg: 'Welcome to Your Vue.js App'
     }
+  },
+  components: {
+    CommitHeader
   },
   mounted() {
     this.$http.get('/api/data.json')
@@ -24,5 +38,19 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="stylus" scoped>
-
+  .header
+   width: 100%
+   height: 1.5rem
+  .content
+   width: 100%
+   height: .8rem 
+   line-height: 0.8rem
+   border-bottom: 0.1px solid #ccc
+   .conItem
+    display: flex
+    .itemList
+     flex: 1
+     display: block
+     font-size: 0.2rem
+     text-align: center
 </style>
---
+++ VueSell/src/page/top/Header.vue
@@ -0,0 +1,19 @@
+
+<template>
+  <div>
+     <div class="header">
+         头部信息
+     </div>
+  </div>
+</template>
+
+<script>
+    export default {
+        name: 'top'
+    }
+</script>
+<style lang='stylus' scoped>
+.header
+ width: 100%
+ height: 1.5rem
+</style>
