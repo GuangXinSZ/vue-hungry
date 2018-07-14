f922bbfef76100813c7cc687fae26f961a865e39
xin <1446854524@qq.com>
2018-07-14 17:48:43 +0800

    修改bug

--- VueSell/config/index.js
+++ VueSell/config/index.js
@@ -12,7 +12,7 @@ module.exports = {
     assetsPublicPath: '/',
     proxyTable: {
       '/api': {
-        target: 'http://192.168.31.121:8080',
+        target: 'http://localhost:8080',
         pathRewrite: {
           '^/api': '/static/mock'
         }
@@ -20,7 +20,7 @@ module.exports = {
     },
 
     // Various Dev Server settings
-    host: '192.168.31.121', // can be overwritten by process.env.HOST
+    host: 'localhost', // can be overwritten by process.env.HOST
     port: 8080, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
     autoOpenBrowser: false,
     errorOverlay: true,
